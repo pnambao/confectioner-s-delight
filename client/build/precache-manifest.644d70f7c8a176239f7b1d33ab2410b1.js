b93cb287394059c4a5d3754f5d1008aa62dfc269
RoachCoach <panjenambao@gmail.com>
2021-12-08 15:22:35 -0600

    added gitignore, build and made changes to some pages and components

---
+++ client/build/precache-manifest.644d70f7c8a176239f7b1d33ab2410b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5905287ea35ff6848a890c4f14ecb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb62163b14bde70810ee",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "a9befd02d2df11dda53d",
+    "url": "/static/js/2.28d357fd.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.28d357fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb62163b14bde70810ee",
+    "url": "/static/js/main.f3ae1f3d.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useSelector, useDispatch } from "react-redux"; 
+// import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
@@ -8,7 +9,12 @@ import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+
+  // To use the store(with redux) useSelector is added
+  const state = useSelector((state)=> state);
+  const dispatch = useDispatch();
+ 
 
   const { currentCategory } = state;
 
