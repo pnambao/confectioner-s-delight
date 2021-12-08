3e6226ae974d9619e9707496d22fafce6898e67f
RoachCoach <panjenambao@gmail.com>
2021-12-08 14:39:23 -0600

    reducer function

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -122,13 +122,64 @@ const reducer = (state = initState, action) => {
           cart: [...state.cart, action.product],
         };
 
+      case ADD_MULTIPLE_TO_CART:
+        return {
+          ...state,
+          cart: [...state.cart, ...action.products],
+        };
 
+        //This case allows us to change the quantity of a certain product/products in the cart.
+        case UPDATE_CART_QUANTITY:
+          return {
+            ...state,
+            cartOpen:true,
+            cart: state.cart.map((product)=>{
+              if (action._id === product._id) {
+                product.purchaseQuantity = action.purchaseQuantity;
+              }
+              return product;
+            }),
+          };
 
+          case REMOVE_FROM_CART:
+            let newState = state.cart.filter((product)  => {
+              return product._id !== action._id;
+            });
 
+            return {
+              ...state,
+              cartOpen: newState.length > 0,
+              cart: newState,
+            };
 
+            case CLEAR_CART:
+              return {
+                ...state,
+                cartOpen: false,
+                cart: [],
+              }:
 
-}
+              case TOGGLE_CART:
+                return {
+                  ...state,
+                  cartOpen: !state.cartOpen,
+                };
 
+                case UPDATE_CATEGORIES:
+                  return {
+                    ...state,
+                    categories: [...action.categories],
+                  };
 
+                 case UPDATE_CURRENT_CATEGORY:
+                   return {
+                     ...state,
+                     currentCategory: action.currentCategory,
+                   };
+                   
+                   default:
+                     return state;
+                    }
+                  }
 
-}
+                  export default reducer;
